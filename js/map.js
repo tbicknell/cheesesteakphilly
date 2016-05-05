d00c543640ea3adb6b8d2b69f135d90584f48467
Tom Bicknell <tbicknell@anthropologie.com>
2016-05-04 23:03:24 -0400

    setting lat & lng to philly

--- js/map.js
+++ js/map.js
@@ -1,7 +1,7 @@
 function initMap() {
     var map;
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
+        center: {lat: 40.0046684, lng: -75.258117},
         zoom: 8
     });
 }
