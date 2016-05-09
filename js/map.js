1fe079fcf7e2c6f83bc7937e729e7be54a0491c1
Tom Bicknell <tbicknell@anthropologie.com>
2016-05-08 21:38:16 -0400

    adding google places param with nearby search and markers

--- js/map.js
+++ js/map.js
@@ -1,10 +1,53 @@
-function initMap() {
+'use strict';
+
+var mapModule = (function() {
     var map;
-    var mapElement = document.getElementById('map');
-    var mapOptions = {
-        zoom: 11,
-        center: { lat: 40.0046684, lng: -75.258117 }, //Philly
-        styles: [{"featureType":"administrative","elementType":"all","stylers":[{"visibility":"on"},{"lightness":33}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2e5d4"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#c5dac6"}]},{"featureType":"poi.park","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":20}]},{"featureType":"road","elementType":"all","stylers":[{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#c5c6c6"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#e4d7c6"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#fbfaf7"}]},{"featureType":"water","elementType":"all","stylers":[{"visibility":"on"},{"color":"#acbcc9"}]}]
+    var infowindow;
+
+    var init = function () {
+        var philly = { lat: 39.9526, lng: -75.1652 };
+        var mapElement = document.getElementById('map');
+        var mapOptions = {
+            zoom: 13,
+            center: philly,
+            styles: [{"featureType":"administrative","elementType":"all","stylers":[{"visibility":"on"},{"lightness":33}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2e5d4"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#c5dac6"}]},{"featureType":"poi.park","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":20}]},{"featureType":"road","elementType":"all","stylers":[{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#c5c6c6"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#e4d7c6"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#fbfaf7"}]},{"featureType":"water","elementType":"all","stylers":[{"visibility":"on"},{"color":"#acbcc9"}]}]
+        };
+        map = new google.maps.Map(mapElement, mapOptions);
+        infowindow = new google.maps.InfoWindow();
+        var service = new google.maps.places.PlacesService(map);
+
+        service.nearbySearch({
+            location: philly,
+            keyword: 'Cheesesteak',
+            radius: 10000,
+        }, nearbySearchCallback);
     };
-    var map = new google.maps.Map(mapElement, mapOptions);
-}
+
+    var nearbySearchCallback = function(results, status) {
+        console.log(results);
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+            for (var i = 0; i < results.length; i++) {
+              createMarker(results[i]);
+            }
+        }
+    };
+
+    var createMarker = function(place) {
+        var placeLoc = place.geometry.location;
+        var marker = new google.maps.Marker({
+            map: map,
+            position: place.geometry.location
+        });
+
+        google.maps.event.addListener(marker, 'click', function() {
+            infowindow.setContent(place.name);
+            infowindow.open(map, this);
+        });
+    };
+
+    return {
+        init: init,
+        nearbySearchCallback: nearbySearchCallback,
+        createMarker: createMarker
+    };
+})();
