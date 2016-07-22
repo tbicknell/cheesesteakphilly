f9178a49eb712fd9bf57cf83e2f14fc23db1d8de
Tom Bicknell <tbicknell@anthropologie.com>
2016-07-21 23:11:49 -0400

    update to padding and zoom

--- js/detail.js
+++ js/detail.js
@@ -8,7 +8,7 @@ var detailModule = (function() {
         var mapElement = document.getElementById('detail-map');
         var mapOptions = {
             center: philly,
-            zoom: 15,
+            zoom: 17,
             styles: [{"featureType":"administrative","elementType":"all","stylers":[{"visibility":"on"},{"lightness":33}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2e5d4"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#c5dac6"}]},{"featureType":"poi.park","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":20}]},{"featureType":"road","elementType":"all","stylers":[{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#c5c6c6"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#e4d7c6"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#fbfaf7"}]},{"featureType":"water","elementType":"all","stylers":[{"visibility":"on"},{"color":"#acbcc9"}]}]
         };
         map = new google.maps.Map(mapElement, mapOptions);
@@ -29,7 +29,7 @@ var detailModule = (function() {
                 position: place.geometry.location
             });
             map.panTo(marker.getPosition());
-            map.setZoom(15);
+            map.setZoom(17);
             render(place);
         }
     };
